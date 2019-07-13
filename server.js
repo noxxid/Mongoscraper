59bb32d46e4231eae72c8a04197604d38f99f340
noxxid <noxxid.john@gmail.com>
2019-07-13 13:20:31 -0600

    changed db name to match

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scraperdb";
 
 mongoose.connect(MONGODB_URI);
 // Routes
